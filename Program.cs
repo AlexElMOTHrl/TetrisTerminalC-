952c483c8992dd0a120363b523d133e97c3a9072
AlexElMOTH <106451459+AlexElMOTHrl@users.noreply.github.com>
2024-11-03 01:09:33 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using TetriosTerminal_Practicar_;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Thread.Sleep(1000);
+		Console.WriteLine("Tetris Iniciando...");
+		Game game = new Game();
+		game.Start();
+	}
+}
